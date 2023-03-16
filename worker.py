adf3ab97d35a21b6260dc2ee8a463a246da48c9c
K Rain Leander <rainleander@users.noreply.github.com>
2023-03-16 13:57:50 -0400

    Create worker.py

---
+++ worker.py
@@ -0,0 +1,17 @@
+from temporal.workflow import WorkflowClient
+from workflow import PokerWorkflow, PokerWorkflowQueries, PokerWorkflowSignals
+from activities import deal_cards, evaluate_hand
+
+class PokerWorkflowWorker:
+    def __init__(self):
+        self.client = WorkflowClient.new_client(namespace='default')
+
+    def run(self):
+        worker = self.client.new_worker(
+            task_queue='poker_workflow', 
+            activities=[deal_cards, evaluate_hand],
+            workflows=[PokerWorkflow.play_poker_workflow],
+            queries=[PokerWorkflowQueries.get_winner],
+            signals=[PokerWorkflowSignals.restart_game]
+        )
+        worker.start()
