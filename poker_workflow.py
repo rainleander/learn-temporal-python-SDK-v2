e572ead40acb5400e6f1aac1b7a35c961c80c74b
K Rain Leander <rainleander@users.noreply.github.com>
2023-04-14 16:18:37 -0400

    Update poker_workflow.py

--- poker_workflow.py
+++ poker_workflow.py
@@ -1,8 +1,8 @@
-import ast
 from temporalio import workflow
+
+from deck_utils import create_deck, deal_cards, shuffle_deck
 from game_state import GameState
-from deck_utils import deal_cards, create_deck, shuffle_deck
-from hand_ranking_workflow import HandRankingWorkflow, HandRankingInput
+from hand_ranking_workflow import HandRankingInput, HandRankingWorkflow
 
 
 @workflow.defn
@@ -16,7 +16,9 @@ class PokerWorkflow:
             game_state.players[i] = await deal_cards(game_state, 5)
 
         for i, player_hand in enumerate(game_state.players):
-            print(f"Player {i + 1}'s hand: {', '.join(str(card) for card in player_hand)}")
+            print(
+                f"Player {i + 1}'s hand: {', '.join(str(card) for card in player_hand)}"
+            )
 
         hand_ranks = [
             await workflow.execute_child_workflow(
@@ -27,8 +29,12 @@ class PokerWorkflow:
         ]
 
         max_rank = max([rank[0] for rank in hand_ranks])
-        winning_hands = [(i, rank) for i, rank in enumerate(hand_ranks) if rank[0] == max_rank]
-        winner_idx, winner_rank = max(winning_hands, key=lambda x: (x[1][0], x[1][1]))
+        winning_hands = [
+            (i, rank) for i, rank in enumerate(hand_ranks) if rank[0] == max_rank
+        ]
+        winner_idx, winner_rank = max(
+            winning_hands, key=lambda x: (x[1][0], x[1][1])
+        )
 
         print(
             f"Player {winner_idx + 1} wins with a {', '.join(str(card) for card in game_state.players[winner_idx])}!"
